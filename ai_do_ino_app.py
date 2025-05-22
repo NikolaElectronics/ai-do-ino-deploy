33a0d30ee18e240962eb17d194e7b8baae136a02
NikolaElectronics <nikolaelectronicshub@gmail.com>
2025-05-22 13:06:06 +0300

    adding using counter

--- ai_do_ino_app.py
+++ ai_do_ino_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.set_page_config(page_title="Test AIdoino", page_icon="⚡")
 
-st.title("✅ Hello from AIdoino")
+st.title("✅ Hello from AIdoino!!!!!!")
 st.write("This is a test to confirm Streamlit runs the correct file.")
 
